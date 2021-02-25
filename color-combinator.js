05ebb403b50d1a20899b9eaa62ba43f92c131f00
cesar juarez <cesar.juarez@codeimmersives.com>
2021-02-25 09:47:18 -0800

    fixed code

--- color-combinator.js
+++ color-combinator.js
@@ -2,10 +2,10 @@
  * YOUR CODE HERE *
  ******************/
 function colorCombinator(color1, color2){
-  if (color1 === "red" || color1 === "yellow" && color2 === "yellow" || color2 ==="red"){
+  if ((color1 === "red" && color2 === "yellow") || (color1 === "yellow" && color2 === "red")){
     return 'orange';
   }
-  if (color1 === "red"  && color2 === "blue"){
+  if ((color1 === "red" && color2 === "blue")||(color1 === "blue" && color2 === "red")){
     return "purple";
   }
   if (color1 === "yellow" || color1 ==="blue" && color2 === "blue" || color2 === "yellow"){
